81e33e6bbdb9cd5770b6115a1992921029e0bd69
Artur D. Silva <artur1512002@gmail.com>
2025-06-12 21:02:10 -0300

    chore: remove unused functions

--- main.py
+++ main.py
@@ -8,48 +8,6 @@ from pieces import Bishop, King, Knight, Pawn, Queen, Rook
 from Color import Color
 
 
-def create_white_pieces():
-    return [
-        Rook((0, 0), Color.WHITE),
-        Knight((1, 0), Color.WHITE),
-        Bishop((2, 0), Color.WHITE),
-        King((3, 0), Color.WHITE),
-        Queen((4, 0), Color.WHITE),
-        Bishop((5, 0), Color.WHITE),
-        Knight((6, 0), Color.WHITE),
-        Rook((7, 0), Color.WHITE),
-        Pawn((0, 1), Color.WHITE),
-        Pawn((1, 1), Color.WHITE),
-        Pawn((2, 1), Color.WHITE),
-        Pawn((3, 1), Color.WHITE),
-        Pawn((4, 1), Color.WHITE),
-        Pawn((5, 1), Color.WHITE),
-        Pawn((6, 1), Color.WHITE),
-        Pawn((7, 1), Color.WHITE),
-    ]
-
-
-def create_black_pieces():
-    return [
-        Rook((0, 7), Color.BLACK),
-        Knight((1, 7), Color.BLACK),
-        Bishop((2, 7), Color.BLACK),
-        King((3, 7), Color.BLACK),
-        Queen((4, 7), Color.BLACK),
-        Bishop((5, 7), Color.BLACK),
-        Knight((6, 7), Color.BLACK),
-        Rook((7, 7), Color.BLACK),
-        Pawn((0, 6), Color.BLACK),
-        Pawn((1, 6), Color.BLACK),
-        Pawn((2, 6), Color.BLACK),
-        Pawn((3, 6), Color.BLACK),
-        Pawn((4, 6), Color.BLACK),
-        Pawn((5, 6), Color.BLACK),
-        Pawn((6, 6), Color.BLACK),
-        Pawn((7, 6), Color.BLACK),
-    ]
-
-
 # function to check all pieces valid options on board
 def check_options(pieces, board):
     all_moves_list = []
